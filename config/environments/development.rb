7a228d338609ba43731a86ce6ca31704b78e58f3
Taehoon Kim <gggkth@naver.com>
2018-02-07 04:05:47 +0000

    Add email with sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,11 +8,13 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://ror-projects-tyhoon.c9users.io' }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
